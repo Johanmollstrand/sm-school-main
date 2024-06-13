5cda664f31dbfe73b997688cdd35fbd799163c27
Johanmollstrand <148531761+Johanmollstrand@users.noreply.github.com>
2024-06-13 23:44:04 +0200

    te

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const MapSection = () => {
   return (
     <MapContainer>
-      <SectionTitle>Our Location</SectionTitle>
+      <SectionTitle>Vår lokal</SectionTitle>
       <iframe
         title="School Location"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345093757!2d144.95565131531813!3d-37.81732797975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf0727eaa71664f0a!2s123%20Traffic%20School%20Lane%2C%20City%2C%20State%2C%20ZIP!5e0!3m2!1sen!2s!4v1629648612386!5m2!1sen!2s"
